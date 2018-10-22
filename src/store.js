1db0215cc283f4517990055a372701ea0a4f62e2
Madyan Eka <madyan.eka95@gmail.com>
2018-10-22 19:45:07 +0700

    storing user data in vuex, create fetch user function

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import globalAxios from 'axios'
 import axios from './axios-auth'
 import { FIREBASE_API_KEY } from '../firebase-config'
 
@@ -8,16 +9,20 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     idToken: null,
-    userId: null
+    userId: null,
+    user: null
   },
   mutations: {
     authUser(state, userData) {
       state.idToken = userData.token
       state.userId = userData.userId
+    },
+    storeUser(state, user) {
+      state.user = user
     }
   },
   actions: {
-    signup({ commit }, authData) {
+    signup({ commit, dispatch }, authData) {
       axios.post(`/signupNewUser?key=${ FIREBASE_API_KEY }`, {
         email: authData.email,
         password: authData.password,
@@ -29,6 +34,7 @@ export default new Vuex.Store({
             token: res.data.idToken,
             userId: res.data.localId
           })
+          dispatch('storeUser', authData)
         })
         .catch(error => console.log(error))
     },
@@ -46,9 +52,38 @@ export default new Vuex.Store({
           })
         })
         .catch(error => console.log(error))
+    },
+    storeUser({ commit, state }, userData) {
+      if (!state.idToken) {
+        return
+      }
+      globalAxios.post('/users.json' + '?auth=' + state.idToken, userData)
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
+    },
+    fetchUser({ commit, state }) {
+      if (!state.idToken) {
+        return
+      }
+      globalAxios.get('/users.json' + '?auth=' + state.idToken)
+        .then(res => {
+          console.log(res)
+          const data = res.data
+          const users = []
+          for (let key in data) {
+            const user = data[key]
+            user.id = key
+            users.push(user)
+          }
+          console.log(users)
+          commit('storeUser', users[0])
+        })
+        .catch(error => console.log(error))
     }
   },
   getters: {
-
+    user(state) {
+      return state.user
+    }
   }
 })
